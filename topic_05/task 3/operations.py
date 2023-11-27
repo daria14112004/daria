890dcca0ba2007cfc528b301787b10a176be8d9f
Дар'я <tumeplaissss1@gmail.com>
2023-11-27 21:43:44 +0200

    updated

---
+++ topic_05/task 3/operations.py
@@ -0,0 +1,18 @@
+# operations.py
+from functions import add, subtract, multiply, divide
+
+class Calculator:
+    def __init__(self):
+        pass
+    
+    def add(self, x, y):
+        return add(x, y)
+
+    def subtract(self, x, y):
+        return subtract(x, y)
+
+    def multiply(self, x, y):
+        return multiply(x, y)
+
+    def divide(self, x, y):
+        return divide(x, y)
